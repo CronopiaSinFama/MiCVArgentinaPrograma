a2e02825666efe596614eff57e5cb6573ba91b29
Mel <meldiazdcv@gmail.com>
2023-02-28 23:05:45 -0300

    modificación javascript

--- scripts.js
+++ scripts.js
@@ -1,1 +1,10 @@
 console.log("Script ready");
+const linkedinFloat = document.getElementById("linkedin-float");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY < 100) {
+    linkedinFloat.style.display = "none";
+  } else {
+    linkedinFloat.style.display = "block";
+  }
+});
